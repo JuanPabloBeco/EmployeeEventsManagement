1e1adf4929520576101489dc0db6b36c62441f78
Juan Pablo Becoña <juanpablobeco@gmail.com>
2023-08-29 16:40:10 -0300

    Delete minute CRON configuration

--- EmployeeEventsManagement/EmployeeEventsManagement/settings_dev.py
+++ EmployeeEventsManagement/EmployeeEventsManagement/settings_dev.py
@@ -6,9 +6,3 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-CRONJOBS = [
-    ('* * * * *', 'events_daily_report.cron.events_daily_report')
-]
-
-EMAIL_DAILY_REPORT_SUBSCRIBERS = ['jpbecona@gmail.com',]
