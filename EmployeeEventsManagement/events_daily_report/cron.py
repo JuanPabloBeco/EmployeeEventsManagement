e025b8121f5ca7a109b7f53bf13f6fd464c95634
Juan Pablo Becoña <juanpablobeco@gmail.com>
2023-08-29 16:49:55 -0300

    Delete log related lines in cron file

--- EmployeeEventsManagement/events_daily_report/cron.py
+++ EmployeeEventsManagement/events_daily_report/cron.py
@@ -3,6 +3,4 @@ from events_daily_report.utils.send_report import send_report
 
 
 def events_daily_report():
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    message = f"Script executed at {timestamp}\n"
     send_report()
